fbe60ee7354886d480becf324c31b61a5e9d953c
szabogaliakos <33122201+szabogaliakos@users.noreply.github.com>
2023-10-02 00:01:40 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-import express from "express";
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const main = async () => {
+  app.listen(8080, () => {
+    console.log("App is listening on 8080");
+    console.log("Try /api/employees route right now");
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
