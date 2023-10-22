3f54b3a8576eaea6ebf99ea2396689ba19f65414
BenedekMartonToth <128388631+BenedekMartonToth@users.noreply.github.com>
2023-10-22 19:07:22 +0200

    add signup logic

--- backend/db/siteUser.model.js
+++ backend/db/siteUser.model.js
@@ -4,21 +4,34 @@ const { Schema } = mongoose;
 
 const SiteUserSchema = new Schema({
   emailAddress: String,
-  phoneNumber: Number,
+  phoneNumber: {
+    type: Number,
+    default: null
+  },
   password: String,
   firstName: String,
   lastName: String,
   profilePhoto: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Image",
+    default: null
   },
   registrationDate: {
     type: Date,
     default: Date.now,
   },
-  lastLoginDate: Date,
-  isBanned: Boolean,
-  twoFactorEnabled: Boolean,
+  lastLoginDate: {
+    type: Date,
+    default: Date.now
+  },
+  isBanned: {
+    type: Boolean,
+    default: false
+  },
+  twoFactorEnabled: {
+    type: Boolean,
+    default: false
+  },
 });
 
 module.exports = mongoose.model("SiteUser", SiteUserSchema);
---
+++ frontend/src/Components/SignUpForm.jsx
@@ -0,0 +1,174 @@
+import { useState, useEffect } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const isEmailValid=(email)=>{
+    const re = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+const checkIfUserIsRegistered = async (email) => {
+    if (isEmailValid(email)) {
+      try {
+        const response = await fetch("DONTLEAVEMEHERE", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email}),
+        });
+  
+        if (response.ok) {
+          const data = await response.json();
+          return data.exists; 
+        } else {
+          throw new Error("Failed to fetch email check");
+        }
+      } catch (error) {
+        console.error(error);
+        return false; 
+      }
+    }
+  };
+
+const SignUp = ({onSignUp}) => {
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
+  const [email, setEmail] = useState("");
+  const [nonHashedPassword, setnonHashedPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [passWordsMatch, setPassWordsMatch] = useState(true);
+  const [passWordStrong, setPassWordStrong] = useState(true);
+  const [emailValid, setEmailValid] = useState(true);
+  const [emailTaken, setEmailTaken] = useState(false);
+
+  const isPasswordValid=(password)=>{
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return passwordPattern.test(password);
+  }
+
+  const errorStatesHandler = (email,password) => {
+
+    if (isEmailValid(email)) {
+        setEmailValid(true);
+      } else if (email.length > 5) {
+        setEmailValid(false);
+      }
+
+    if (checkIfUserIsRegistered(email)) {
+        setEmailTaken(true);
+      } else if (email.length > 5) {
+        setEmailTaken(false);
+      }
+
+    if (nonHashedPassword === confirmPassword) {
+      setPassWordsMatch(true);
+    } else if (confirmPassword.length > 5) {
+      setPassWordsMatch(false);
+    }
+
+    if (isPasswordValid(password)) {
+      setPassWordStrong(true);
+    } else if (nonHashedPassword.length > 5) {
+      setPassWordStrong(false);
+    }
+  };
+
+  const onSubmit = (e)=> {
+    e.preventDefault();
+
+    return onSignUp({
+        firstName,
+        lastName,
+        email,
+        nonHashedPassword,
+    }
+    )
+  }
+
+  useEffect(() => {
+    errorStatesHandler(email,nonHashedPassword);
+  }, [email,nonHashedPassword, confirmPassword]);
+
+  return (
+    <>
+      <div>Sign up here:</div>
+      {!emailValid && (
+        <div style={{ color: "red" }}>Invalid E-mail address!</div>
+      )}
+      {!emailTaken && (
+        <div style={{ color: "red" }}>E-mail address already in use!</div>
+      )}
+      {!passWordsMatch && (
+        <div style={{ color: "red" }}>The passwords do not match!</div>
+      )}
+      {!passWordStrong && (
+        <div style={{ color: "red" }}>The password is too weak!</div>
+      )}
+
+      <form className="SignUpFields" onSubmit={onSubmit}>
+        <div className="control">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            onChange={(e) => setFirstName(e.target.value)}
+            name="firstName"
+            id="firstName"
+          />
+        </div>
+
+        <div className="control">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            onChange={(e) => setLastName(e.target.value)}
+            name="lastName"
+            id="lastName"
+          />
+        </div>
+
+        <div className="control">
+          <label htmlFor="email">E-mail:</label>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            id="email"
+          />
+        </div>
+
+        <div className="control">
+          <label htmlFor="nonHashedPassword">Password:</label>
+          <input
+            type="password"
+            onChange={(e) => setnonHashedPassword(e.target.value)}
+            name="nonHashedPassword"
+            id="nonHashedPassword"
+          />
+        </div>
+
+        <div className="control">
+          <label htmlFor="confirmPassword">Confrim password:</label>
+          <input
+            type="password"
+            onChange={(e) => setconfirmPassword(e.target.value)}
+            name="confirmPassword"
+            id="confirmPassword"
+          />
+        </div>
+
+        {emailValid && passWordsMatch && passWordStrong && (
+          <div className="button">
+            <button type="submit">Sign up!</button>
+          </div>
+        )}
+
+        <li>
+          <Link to="/faq">
+            <button type="button">Need help?</button>
+          </Link>
+        </li>
+
+      </form>
+    </>
+  );
+};
+
+export default SignUp;
