b06591755eac9bae85769e556480ef74f5f22dfd
martonteker <teker.marton@gmail.com>
2023-11-02 20:27:17 +0100

    Professionalism & morale boost hotfix

--- frontend/src/Components/Layout/Footer.jsx
+++ frontend/src/Components/Layout/Footer.jsx
@@ -2,11 +2,11 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <div className="footer__left">Gyurcsány Ferenc</div>
+        <div className="footer__left">🔥</div>
 
-        <div className="footer__center">To Do</div>
+        <div className="footer__center">Go Average JS Enjoyer Team!</div>
 
-        <div className="footer__right">Orbán Viktor</div>
+        <div className="footer__right">🔥</div>
       </div>
     </footer>
   );
