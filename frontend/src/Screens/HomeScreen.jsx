a7bc3ffa1774f85709d456cf81fd368e2e7608d0
martonteker <teker.marton@gmail.com>
2023-11-06 19:03:06 +0100

    Added buttons and status messages (SCSS + anim).

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -3,94 +3,46 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "context/AuthContext";
 
-import SignOutButton from "components/forms/SignOutButton";
+import Button from "components/common/Buttons";
+import StatusMessage from "components/common/StatusMessage";
 
 const HomeScreen = () => {
   const { isAuthenticated, user, signIn, signOut } = useAuth();
 
-  const location = useLocation(); // Use the useLocation hook here
+  const location = useLocation();
+
   // Check for the navigation state
   const { signInSuccess, signOutSuccess } = location.state || {};
 
   return (
     <div className="Layout">
       {signInSuccess && (
-        <div
-          style={{
-            color: "#155724",
-            backgroundColor: "#d4edda",
-            borderColor: "#c3e6cb",
-            padding: "0.75rem 1.25rem",
-            marginBottom: "1rem",
-            border: "1px solid transparent",
-            borderRadius: "0.25rem",
-            fontSize: "2rem",
-            lineHeight: 1.5,
-          }}
-        >
-          Welcome back! You have signed in successfully.
-        </div>
+        <StatusMessage
+          type="success"
+          message="Welcome back! You have signed in successfully."
+        />
       )}
 
       {signOutSuccess && (
-        <div
-          style={{
-            color: "#0c5460",
-            backgroundColor: "#d1ecf1",
-            borderColor: "#bee5eb",
-            padding: "0.75rem 1.25rem",
-            marginBottom: "1rem",
-            border: "1px solid transparent",
-            borderRadius: "0.25rem",
-            fontSize: "2rem",
-            lineHeight: 1.5,
-          }}
-        >
-          Successfully signed out!
-        </div>
+        <StatusMessage
+          type="info"
+          message="You have signed out successfully. See you soon!"
+        />
       )}
 
-      <nav>
-        <ul>
-          <li>
-            <SignOutButton />
-          </li>
-          <li>
-            <Link to="/ui-template">
-              <button
-                type="button"
-                style={{
-                  border: "1px solid black",
-                  padding: "5px 10px",
-                  borderRadius: "5px",
-                }}
-              >
-                Go To UI Templates
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/signup">
-              <button type="button" style={{ padding: "5px 10px" }}>
-                Sign Up!
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/profile/edit">
-              <button type="button" style={{ padding: "5px 10px" }}>
-                Userdata!
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/signin">
-              <button type="button" style={{ padding: "5px 10px" }}>
-                Sign in!
-              </button>
-            </Link>
-          </li>
-        </ul>
+      <nav style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <Link to="/signup">
+          <Button variant="outline-secondary btn--compact">Sign Up</Button>
+        </Link>
+        <Link to="/signin">
+          <Button variant="outline-primary btn--compact">Sign In</Button>
+        </Link>
+        <Link to="/profile/edit">
+          <Button variant="info btn--compact">Edit Profile</Button>
+        </Link>
+        <Button variant="outline-dark btn--compact" onClick={signOut}>
+          Sign Out
+        </Button>
       </nav>
     </div>
   );
