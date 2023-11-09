309063f44b36dbbd91c6c7b28310e469483cc8fa
martonteker <teker.marton@gmail.com>
2023-11-09 21:36:31 +0100

    Implemented oauth state check

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -64,7 +64,8 @@ export const useAuth = () => {
     const codeVerifier = generateCodeVerifier();
     const codeChallenge = generateCodeChallenge(codeVerifier);
 
-    // Save the codeVerifier in sessionStorage or other secure place to use later
+    // Save the state and codeVerifier in sessionStorage or other secure place to use later
+    sessionStorage.setItem(`${provider}State`, state);
     sessionStorage.setItem(`${provider}CodeVerifier`, codeVerifier);
 
     const authUrl = getAuthUrl(provider, state, codeChallenge);
