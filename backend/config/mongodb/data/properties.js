72cacdfed9c53a7e0d9a403e94aafc07e42376ed
Akos Szabo-Gali <szabogaliakos@gmail.com>
2023-11-11 17:20:24 +0100

    Property model and seed added

---
+++ backend/config/mongodb/data/properties.js
@@ -0,0 +1,677 @@
+module.exports = [
+  {
+    _id: '654fa6272a455bd2a360cc70',
+    name: 'processor',
+    category_id: '507f1f77bcf86cd799439013',
+  },
+  {
+    _id: '654fa6272a455bd2a360cc71',
+    name: 'RAM',
+    category_id: '507f1f77bcf86cd799439013',
+  },
+  {
+    _id: '654fa6272a455bd2a360cc72',
+    name: 'storage',
+    category_id: '507f1f77bcf86cd799439013',
+  },
+  {
+    _id: '654fa6272a455bd2a360cc73',
+    name: 'graphics_card',
+    category_id: '507f1f77bcf86cd799439013',
+  },
+  {
+    _id: '654fa6272a455bd2a360cc74',
+    name: 'cooling',
+    category_id: '507f1f77bcf86cd799439013',
+  },
+  {
+    _id: '654fa6272a455bd2a360cc89',
+    name: 'processor',
+    category_id: '507f1f77bcf86cd799439014',
+  },
+  {
+    _id: '654fa6272a455bd2a360cc8a',
+    name: 'RAM',
+    category_id: '507f1f77bcf86cd799439014',
+  },
+  {
+    _id: '654fa6272a455bd2a360cc8b',
+    name: 'storage',
+    category_id: '507f1f77bcf86cd799439014',
+  },
+  {
+    _id: '654fa6272a455bd2a360cc8c',
+    name: 'graphics_card',
+    category_id: '507f1f77bcf86cd799439014',
+  },
+  {
+    _id: '654fa6272a455bd2a360cc8d',
+    name: 'form_factor',
+    category_id: '507f1f77bcf86cd799439014',
+  },
+  {
+    _id: '654fa6272a455bd2a360cca2',
+    name: 'processor',
+    category_id: '507f1f77bcf86cd799439015',
+  },
+  {
+    _id: '654fa6272a455bd2a360cca3',
+    name: 'RAM',
+    category_id: '507f1f77bcf86cd799439015',
+  },
+  {
+    _id: '654fa6272a455bd2a360cca4',
+    name: 'storage',
+    category_id: '507f1f77bcf86cd799439015',
+  },
+  {
+    _id: '654fa6272a455bd2a360cca5',
+    name: 'graphics_card',
+    category_id: '507f1f77bcf86cd799439015',
+  },
+  {
+    _id: '654fa6272a455bd2a360cca6',
+    name: 'security',
+    category_id: '507f1f77bcf86cd799439015',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccbb',
+    name: 'base_clock',
+    category_id: '507f1f77bcf86cd799439017',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccbc',
+    name: 'boost_clock',
+    category_id: '507f1f77bcf86cd799439017',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccbd',
+    name: 'cores',
+    category_id: '507f1f77bcf86cd799439017',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccbe',
+    name: 'threads',
+    category_id: '507f1f77bcf86cd799439017',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccbf',
+    name: 'TDP',
+    category_id: '507f1f77bcf86cd799439017',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccd4',
+    name: 'socket',
+    category_id: '507f1f77bcf86cd799439018',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccd5',
+    name: 'chipset',
+    category_id: '507f1f77bcf86cd799439018',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccd6',
+    name: 'RAM_slots',
+    category_id: '507f1f77bcf86cd799439018',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccd7',
+    name: 'max_RAM',
+    category_id: '507f1f77bcf86cd799439018',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccd8',
+    name: 'form_factor',
+    category_id: '507f1f77bcf86cd799439018',
+  },
+  {
+    _id: '654fa6272a455bd2a360cced',
+    name: 'capacity',
+    category_id: '507f1f77bcf86cd799439019',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccee',
+    name: 'speed',
+    category_id: '507f1f77bcf86cd799439019',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccef',
+    name: 'CAS_latency',
+    category_id: '507f1f77bcf86cd799439019',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccf0',
+    name: 'voltage',
+    category_id: '507f1f77bcf86cd799439019',
+  },
+  {
+    _id: '654fa6272a455bd2a360ccf1',
+    name: 'type',
+    category_id: '507f1f77bcf86cd799439019',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd06',
+    name: 'key_switch',
+    category_id: '507f1f77bcf86cd79943901b',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd07',
+    name: 'backlight',
+    category_id: '507f1f77bcf86cd79943901b',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd08',
+    name: 'keycap_material',
+    category_id: '507f1f77bcf86cd79943901b',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd09',
+    name: 'connection_type',
+    category_id: '507f1f77bcf86cd79943901b',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd0a',
+    name: 'layout',
+    category_id: '507f1f77bcf86cd79943901b',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd1f',
+    name: 'resolution',
+    category_id: '507f1f77bcf86cd79943901d',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd20',
+    name: 'refresh_rate',
+    category_id: '507f1f77bcf86cd79943901d',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd21',
+    name: 'panel_type',
+    category_id: '507f1f77bcf86cd79943901d',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd22',
+    name: 'response_time',
+    category_id: '507f1f77bcf86cd79943901d',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd23',
+    name: 'connectivity',
+    category_id: '507f1f77bcf86cd79943901d',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd38',
+    name: 'DPI',
+    category_id: '507f1f77bcf86cd79943901c',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd39',
+    name: 'buttons',
+    category_id: '507f1f77bcf86cd79943901c',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd3a',
+    name: 'connection_type',
+    category_id: '507f1f77bcf86cd79943901c',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd3b',
+    name: 'sensor',
+    category_id: '507f1f77bcf86cd79943901c',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd3c',
+    name: 'ergonomics',
+    category_id: '507f1f77bcf86cd79943901c',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd51',
+    name: 'power',
+    category_id: '507f1f77bcf86cd799439020',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd52',
+    name: 'capacity',
+    category_id: '507f1f77bcf86cd799439020',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd53',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439020',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd54',
+    name: 'control',
+    category_id: '507f1f77bcf86cd799439020',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd55',
+    name: 'features',
+    category_id: '507f1f77bcf86cd799439020',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd6a',
+    name: 'burners',
+    category_id: '507f1f77bcf86cd799439021',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd6b',
+    name: 'material',
+    category_id: '507f1f77bcf86cd799439021',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd6c',
+    name: 'size',
+    category_id: '507f1f77bcf86cd799439021',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd6d',
+    name: 'control',
+    category_id: '507f1f77bcf86cd799439021',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd6e',
+    name: 'fuel_type',
+    category_id: '507f1f77bcf86cd799439021',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd83',
+    name: 'slices',
+    category_id: '507f1f77bcf86cd799439022',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd84',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439022',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd85',
+    name: 'power',
+    category_id: '507f1f77bcf86cd799439022',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd86',
+    name: 'material',
+    category_id: '507f1f77bcf86cd799439022',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd87',
+    name: 'settings',
+    category_id: '507f1f77bcf86cd799439022',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd9c',
+    name: 'capacity',
+    category_id: '507f1f77bcf86cd799439024',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd9d',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439024',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd9e',
+    name: 'energy_rating',
+    category_id: '507f1f77bcf86cd799439024',
+  },
+  {
+    _id: '654fa6272a455bd2a360cd9f',
+    name: 'number_of_shelves',
+    category_id: '507f1f77bcf86cd799439024',
+  },
+  {
+    _id: '654fa6272a455bd2a360cda0',
+    name: 'freezer_capacity',
+    category_id: '507f1f77bcf86cd799439024',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdb5',
+    name: 'capacity',
+    category_id: '507f1f77bcf86cd799439026',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdb6',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439026',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdb7',
+    name: 'temperature_range',
+    category_id: '507f1f77bcf86cd799439026',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdb8',
+    name: 'number_of_racks',
+    category_id: '507f1f77bcf86cd799439026',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdb9',
+    name: 'dimensions',
+    category_id: '507f1f77bcf86cd799439026',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdce',
+    name: 'capacity',
+    category_id: '507f1f77bcf86cd799439025',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdcf',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439025',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdd0',
+    name: 'energy_rating',
+    category_id: '507f1f77bcf86cd799439025',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdd1',
+    name: 'number_of_shelves',
+    category_id: '507f1f77bcf86cd799439025',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdd2',
+    name: 'dimensions',
+    category_id: '507f1f77bcf86cd799439025',
+  },
+  {
+    _id: '654fa6272a455bd2a360cde7',
+    name: 'capacity',
+    category_id: '507f1f77bcf86cd799439028',
+  },
+  {
+    _id: '654fa6272a455bd2a360cde8',
+    name: 'energy_rating',
+    category_id: '507f1f77bcf86cd799439028',
+  },
+  {
+    _id: '654fa6272a455bd2a360cde9',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439028',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdea',
+    name: 'drying_type',
+    category_id: '507f1f77bcf86cd799439028',
+  },
+  {
+    _id: '654fa6272a455bd2a360cdeb',
+    name: 'features',
+    category_id: '507f1f77bcf86cd799439028',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce00',
+    name: 'capacity',
+    category_id: '507f1f77bcf86cd799439029',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce01',
+    name: 'energy_rating',
+    category_id: '507f1f77bcf86cd799439029',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce02',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439029',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce03',
+    name: 'spin_speed',
+    category_id: '507f1f77bcf86cd799439029',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce04',
+    name: 'features',
+    category_id: '507f1f77bcf86cd799439029',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce19',
+    name: 'capacity',
+    category_id: '507f1f77bcf86cd79943902a',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce1a',
+    name: 'energy_rating',
+    category_id: '507f1f77bcf86cd79943902a',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce1b',
+    name: 'color',
+    category_id: '507f1f77bcf86cd79943902a',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce1c',
+    name: 'spin_speed',
+    category_id: '507f1f77bcf86cd79943902a',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce1d',
+    name: 'features',
+    category_id: '507f1f77bcf86cd79943902a',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce32',
+    name: 'memory',
+    category_id: '507f1f77bcf86cd79943902d',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce33',
+    name: 'color',
+    category_id: '507f1f77bcf86cd79943902d',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce34',
+    name: 'screen_size',
+    category_id: '507f1f77bcf86cd79943902d',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce35',
+    name: 'processor',
+    category_id: '507f1f77bcf86cd79943902d',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce36',
+    name: 'graphics_card',
+    category_id: '507f1f77bcf86cd79943902d',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce4b',
+    name: 'memory',
+    category_id: '507f1f77bcf86cd79943902e',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce4c',
+    name: 'color',
+    category_id: '507f1f77bcf86cd79943902e',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce4d',
+    name: 'screen_size',
+    category_id: '507f1f77bcf86cd79943902e',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce4e',
+    name: 'processor',
+    category_id: '507f1f77bcf86cd79943902e',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce4f',
+    name: 'storage',
+    category_id: '507f1f77bcf86cd79943902e',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce64',
+    name: 'memory',
+    category_id: '507f1f77bcf86cd79943902f',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce65',
+    name: 'color',
+    category_id: '507f1f77bcf86cd79943902f',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce66',
+    name: 'screen_size',
+    category_id: '507f1f77bcf86cd79943902f',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce67',
+    name: 'screen_technology',
+    category_id: '507f1f77bcf86cd79943902f',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce68',
+    name: 'battery_life',
+    category_id: '507f1f77bcf86cd79943902f',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce7d',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439031',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce7e',
+    name: 'material',
+    category_id: '507f1f77bcf86cd799439031',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce7f',
+    name: 'compatible_models',
+    category_id: '507f1f77bcf86cd799439031',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce80',
+    name: 'features',
+    category_id: '507f1f77bcf86cd799439031',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce81',
+    name: 'brand',
+    category_id: '507f1f77bcf86cd799439031',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce96',
+    name: 'memory',
+    category_id: '507f1f77bcf86cd799439033',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce97',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439033',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce98',
+    name: 'screen_size',
+    category_id: '507f1f77bcf86cd799439033',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce99',
+    name: 'battery_life',
+    category_id: '507f1f77bcf86cd799439033',
+  },
+  {
+    _id: '654fa6272a455bd2a360ce9a',
+    name: 'camera_resolution',
+    category_id: '507f1f77bcf86cd799439033',
+  },
+  {
+    _id: '654fa6272a455bd2a360ceaf',
+    name: 'memory',
+    category_id: '507f1f77bcf86cd799439032',
+  },
+  {
+    _id: '654fa6272a455bd2a360ceb0',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439032',
+  },
+  {
+    _id: '654fa6272a455bd2a360ceb1',
+    name: 'screen_size',
+    category_id: '507f1f77bcf86cd799439032',
+  },
+  {
+    _id: '654fa6272a455bd2a360ceb2',
+    name: 'battery_life',
+    category_id: '507f1f77bcf86cd799439032',
+  },
+  {
+    _id: '654fa6272a455bd2a360ceb3',
+    name: 'camera_resolution',
+    category_id: '507f1f77bcf86cd799439032',
+  },
+  {
+    _id: '654fa6272a455bd2a360cec8',
+    name: 'memory',
+    category_id: '507f1f77bcf86cd799439035',
+  },
+  {
+    _id: '654fa6272a455bd2a360cec9',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439035',
+  },
+  {
+    _id: '654fa6272a455bd2a360ceca',
+    name: 'battery_life',
+    category_id: '507f1f77bcf86cd799439035',
+  },
+  {
+    _id: '654fa6272a455bd2a360cecb',
+    name: 'strap_material',
+    category_id: '507f1f77bcf86cd799439035',
+  },
+  {
+    _id: '654fa6272a455bd2a360cecc',
+    name: 'display_size',
+    category_id: '507f1f77bcf86cd799439035',
+  },
+  {
+    _id: '654fa6272a455bd2a360cee1',
+    name: 'memory',
+    category_id: '507f1f77bcf86cd799439036',
+  },
+  {
+    _id: '654fa6272a455bd2a360cee2',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439036',
+  },
+  {
+    _id: '654fa6272a455bd2a360cee3',
+    name: 'battery_life',
+    category_id: '507f1f77bcf86cd799439036',
+  },
+  {
+    _id: '654fa6272a455bd2a360cee4',
+    name: 'strap_material',
+    category_id: '507f1f77bcf86cd799439036',
+  },
+  {
+    _id: '654fa6272a455bd2a360cee5',
+    name: 'display_size',
+    category_id: '507f1f77bcf86cd799439036',
+  },
+  {
+    _id: '654fa6272a455bd2a360cefa',
+    name: 'memory',
+    category_id: '507f1f77bcf86cd799439037',
+  },
+  {
+    _id: '654fa6272a455bd2a360cefb',
+    name: 'color',
+    category_id: '507f1f77bcf86cd799439037',
+  },
+  {
+    _id: '654fa6272a455bd2a360cefc',
+    name: 'battery_life',
+    category_id: '507f1f77bcf86cd799439037',
+  },
+  {
+    _id: '654fa6272a455bd2a360cefd',
+    name: 'strap_material',
+    category_id: '507f1f77bcf86cd799439037',
+  },
+  {
+    _id: '654fa6272a455bd2a360cefe',
+    name: 'water_resistance',
+    category_id: '507f1f77bcf86cd799439037',
+  },
+];
--- backend/config/mongodb/data/transform.js
+++ backend/config/mongodb/data/transform.js
@@ -3,13 +3,46 @@ const clipboardy = require('node-clipboardy');
 
 const products = require('./products');
 
-// Add ObjectID to each product
-const productsWithObjectID = products.map((product) => ({
-  _id: new ObjectId().toString(),
-  ...product,
-}));
+// // Add ObjectID to each product
+// const productsWithObjectID = products.map((product) => ({
+//   _id: new ObjectId().toString(),
+//   name: product.name,
+//   description: product.description,
+//   category_id: product.category_id,
+// }));
 
-const jsArrayString = JSON.stringify(productsWithObjectID, null, 2);
+// const jsArrayString = JSON.stringify(productsWithObjectID, null, 2);
+
+const properties = products.flatMap((product) =>
+  Object.entries(product)
+    .filter(
+      ([key]) => !['_id', 'name', 'description', 'category_id'].includes(key),
+    )
+    .map(([key, value]) => ({
+      _id: new ObjectId().toString(),
+      name: key,
+      category_id: product.category_id,
+    })),
+);
+
+const distinctProperties = (properties) => {
+  const uniqueCombinations = new Set();
+
+  return properties.filter((property) => {
+    const key = `${property.name}-${property.category_id}`;
+
+    if (!uniqueCombinations.has(key)) {
+      uniqueCombinations.add(key);
+      return true;
+    }
+
+    return false;
+  });
+};
+
+const distinctPropertiesArray = distinctProperties(properties);
+
+const jsArrayString = JSON.stringify(distinctPropertiesArray, null, 2);
 
 clipboardy.writeSync(jsArrayString);
 
